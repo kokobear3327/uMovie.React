9ff648be35b2c5426c0296da766436c53ba56779
RichardPitts1 <Richardepitts@gmail.com>
2020-07-06 21:15:57 -0500

    1

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,10 +22,19 @@ import { useHomeFetch } from './hooks/useHomeFetch';
 
 const Home = () => {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  
   console.log(state);
+  
+  if (error) return <div>Something went wrong ...</div>;
+  if (!state.movies[0]) return <Spinner />
+  
     return (
         <>
-        <HeroImage />
+        <HeroImage 
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}    
+            title={state.heroImage.original_title}
+            text={state.heroImage.overview}
+            />
         <SearchBar />
         <Grid />
         <MovieThumb />
